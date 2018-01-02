8bc2df65da373db54c991c36aaa4e870717e3c65
NaveedUK <naveed@middleoffice.com>
2018-01-02 16:17:29 +0000

    Add files via upload

---
+++ 1-scrape.py
@@ -0,0 +1,30 @@
+# Naveed - Scrape to pull content from BBC Judo site. 
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import io
+
+
+
+url = 'http://www.bbc.co.uk/sport/judo'
+
+browser = webdriver.Firefox()
+
+browser.get(url)
+
+def get_html_data():
+    html = browser.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    filename = str('bbc-judo') + '.html'
+    
+    with io.open(filename, "w", encoding="utf-8") as f:
+        f.write(str(soup))
+        
+    print(str(url) + '  Done')
+    print()
+    print(str(filename) + ' saved locally')
+
+
+get_html_data()
+
+browser.close()
